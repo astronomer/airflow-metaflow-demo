80548e38cc301cfc9417bd8c372a5a45b7120ec0
Michael Gregory <>
2023-05-28 12:40:20 +0200

    cleanup

--- include/foreach.py
+++ include/foreach.py
@@ -1,7 +1,10 @@
-from metaflow import FlowSpec, step
+from metaflow import FlowSpec, step, environment
+
+pod_env_vars={"AWS_ACCESS_KEY_ID": "admin", "AWS_SECRET_ACCESS_KEY": "adminadmin"}
 
 class ForeachFlow(FlowSpec):
 
+    @environment(pod_env_vars)
     @step
     def start(self):
         self.titles = ['Stranger Things',
@@ -9,16 +12,19 @@ class ForeachFlow(FlowSpec):
                        'Narcos']
         self.next(self.a, foreach='titles')
     
+    @environment(pod_env_vars)
     @step
     def a(self):
         self.title = '%s processed' % self.input
         self.next(self.join)
 
+    @environment(pod_env_vars)
     @step
     def join(self, inputs):
         self.results = [input.title for input in inputs]
         self.next(self.end)
 
+    @environment(pod_env_vars)
     @step
     def end(self):
         print('\n'.join(self.results))
